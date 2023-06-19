131e8e3513371553167b969e617b15c59055c334
kanchanchavda <136856057+kanchanchavda@users.noreply.github.com>
2023-06-19 09:17:50 -0600

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -1,4 +1,4 @@
 import stremlit
 streamlit.title ("My parents New Healthy Diner')
-streamlit.hearder("Breakfast menu"
+streamlit.hearder("Breakfast menu")
 streamlit.text ("Omega 3 and Blue berry Oatmeal")
