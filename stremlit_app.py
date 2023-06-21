0f4f1d2ece19594b6483bc39463af72becfb44d4
kanchanchavda <136856057+kanchanchavda@users.noreply.github.com>
2023-06-21 09:16:25 -0600

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -15,7 +15,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+" Kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + "Kiwi")
 
 
 # write your own comment -what does the next line do? 
